159f7e2359e9a8b8fb97ac7e1cb8d7c5c87abee9
CristianCV <cristiancv2000@hotmail.com>
2024-02-09 12:18:15 -0800

    firts commit

---
+++ Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActOne
+{
+    [System.Runtime.InteropServices.ComVisible(true)]
+    public class Class1 : Xarial.XCad.SolidWorks.SwAddInEx
+    {
+        public override void OnConnect()
+        {
+            Application.ShowMessageBox("Hola Julian");
+        }
+    }
+}
